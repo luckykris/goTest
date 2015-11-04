34b60cf1ed5545f4ea0504345574bed109ac7d92
root <root@dev-zabbix-server-001.(none)>
2015-11-04 12:49:51 +0800

    update

--- internalFunction.go
+++ internalFunction.go
@@ -2,8 +2,28 @@ package main
 import (
 	"fmt"
 )
+const(
+	A=1<<iota
+	B
+	C
+)
+const(
+	D=1
+	E=iota //iota is index of the 'const zone'
+	F
+)
+
 func main(){
-	Cap()
+	Const()
+	//Cap()
+}
+func Const(){
+	fmt.Println(A)
+	fmt.Println(B)
+	fmt.Println(C)
+	fmt.Println(D)
+	fmt.Println(E)
+	fmt.Println(F)
 }
 func Cap(){
 	a:=make(chan int,10)
